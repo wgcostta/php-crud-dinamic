2805020ca8bb24a4d4cc30ace8f8f74c2701f4c1
Wagner <wg.o.costa@gmail.com>
2020-07-02 22:19:22 -0300

    Style no Index.php

--- index.php
+++ index.php
@@ -2,23 +2,45 @@
 
 	$paginas = ['home'=>'Minha página home aqui!',
 				'sobre'=>'Estou na pagina sobre',
-				'contato'=>'<input type="text" />'];
-
+				'contato'=>''];
+	$paginas['contato'] = '<form><input type="text" placeholder="Seu nome..."/></form>'
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>
-		Tutorial Dinamico
+	<title>Tutorial Dinamico</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		header{
+			background-color: #069;
+			padding: 8px 10px;
+			text-align: center; 
+		}
+		a{
+			display:inline-block;
+			margin:0 10px;
+			color: white;
+			font-size: 17px;
 
-	</title>
+		}
+	</style>
 </head>
 <body>
-	<?php
-		echo $paginas['contato'];
-	?>
+	<header>
+		<?php
+			foreach ($paginas as $key => $value) {
+				echo '<a href="?page='.$key.'">'.$key.'</a>';
+			}
+			
+		?>
+	</header>
+
 </body>
 
 
