80bbf13286b89c3489a9f84946ccc57c7d9f1e9f
Wagner <wg.o.costa@gmail.com>
2020-07-02 22:04:13 -0300

    Criando página simples em PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+	$paginas = ['home','sobre','contato'];
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Tutorial Dinamico
+
+	</title>
+</head>
+<body>
+	<?php
+		echo $paginas[0];
+	?>
+</body>
+
+
+</html>
